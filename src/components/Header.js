a22dbad11b28f021e6aa2262f96eaf2b8672c7b9
amorphous786 <usamanadeem0786@gmail.com>
2023-02-11 12:17:56 +0500

    THIS IS ECOMMERCE SETUP

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+function Header() {
+  return (
+      <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                  <Container>
+                        <Navbar.Brand href="/">LuffyShop</Navbar.Brand>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                        <Navbar.Collapse id='basic-navbar-nav'>
+                              <Nav className="m -auto">
+                                    <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                                    <Nav.Link href='/user'><i className='fas fa-user'></i>Sign In</Nav.Link>
+
+                              </Nav>
+                        </Navbar.Collapse>
+                  </Container>
+            </Navbar>
+      </header>
+  )
+}
+
+export default Header
